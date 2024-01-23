44601ad2702cc9ff4a8e40f1213b5b9ee2a688b4
GillesR76 <gilles-richard76@hotmail.com>
2024-01-23 15:22:39 +0100

    print names from compiled module

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    print("{}".format(hidden_4))
+    for i in dir(hidden_4):
+        if i != "__":
+            print("{}".format(i))
