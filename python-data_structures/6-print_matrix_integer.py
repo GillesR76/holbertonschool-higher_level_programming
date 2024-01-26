6ce6f11e262910ef016c2a78a1620685a1a66deb
GillesR76 <gilles-richard76@hotmail.com>
2024-01-26 17:04:22 +0100

    removing space at the end

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for column in row:
             print("{:d}".format(column), end="")
+            if column != row[-1]:
+            	print(" ".format(column), end="")
         print()
