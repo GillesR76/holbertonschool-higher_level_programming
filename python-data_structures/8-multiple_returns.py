230119dbee20727a3076321bb6404c9cd402b677
GillesR76 <gilles-richard76@hotmail.com>
2024-01-26 21:52:49 +0100

    updating the function

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None or len(sentence) == 0:
+    if len(sentence) == 0:
         return(len(sentence), "None")
     else:
         return (len(sentence), sentence[0])
