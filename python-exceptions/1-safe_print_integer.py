5fb6643f7fde40353925de52a213d8603e5329bf
GillesR76 <gilles-richard76@hotmail.com>
2024-01-31 07:06:35 +0100

    excluding non integers without using type function

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if isinstance(value, int):
-            print("{:d}".format(value))
-            return True
+        print("{:d}".format(value))
+        return True
     except Exception:
-        pass
+        return False
