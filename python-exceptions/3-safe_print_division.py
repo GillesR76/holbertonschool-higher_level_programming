779224f399d44b64684fe0d8200159f6bbdf80e5
GillesR76 <gilles-richard76@hotmail.com>
2024-01-31 15:44:15 +0100

    divide 2 integers with exception

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,10 +3,11 @@ def safe_print_division(a, b):
     calculate = 0
     try:
         calculate = a / b
-        if a or b > 0:
+        if a and b > 0:
             print("Inside result: {}".format(float(calculate)))
     except Exception:
         print("Inside result: None")
         pass
     finally:
-        return calculate
+        if a and b > 0:
+            return calculate
