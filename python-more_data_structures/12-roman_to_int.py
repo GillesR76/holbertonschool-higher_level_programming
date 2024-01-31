f36269c3d8b69814895196c83b1ce14b0daf9e21
GillesR76 <gilles-richard76@hotmail.com>
2024-01-31 04:00:45 +0100

    convert roman numerals to integers

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     rom_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
                   "D": 500, "M": 1000}
-    if not roman_string or roman_string == None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
     result = 0
     for i in range(len(roman_string)):
