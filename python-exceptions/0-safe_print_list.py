a87d183bf77a5d5f2dfdb6d7b5b12f009eb31944
GillesR76 <gilles-richard76@hotmail.com>
2024-01-31 14:40:44 +0100

    updating safe print list and adding the print and count function

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,9 +2,8 @@
 def safe_print_list(my_list=[], x=0):
     try:
         count = 0
-        for i in my_list:
-            if count < x:
-                print("{}".format(i), end="")
+        for i in range(0, x):
+                print("{}".format(my_list[i]), end="")
                 count += 1
     except Exception:
         pass
