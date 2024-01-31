3b13834278899a124b3b146ce4c1f468165516ac
GillesR76 <gilles-richard76@hotmail.com>
2024-01-31 22:37:24 +0100

    creating two variables to reduce line

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    rom_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
-                  "D": 500, "M": 1000}
+    dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
+            "D": 500, "M": 1000}
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
     result = 0
     for i in range(len(roman_string)):
-        if i > 0 and rom_dict[roman_string[i]] > rom_dict[roman_string[i - 1]]:
-            result += rom_dict[roman_string[i]] - 2 * rom_dict[roman_string[i - 1]]
+        current = roman_string[i]
+        previous = roman_string[i - 1]
+        if i > 0 and dict[current] > dict[previous]:
+            result += dict[current] - 2 * dict[previous]
         else:
-            result += rom_dict[roman_string[i]]
+            result += dict[current]
     return result
