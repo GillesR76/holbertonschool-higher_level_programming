016ae9e0c85d899a7ad1df4c1716fefb8df76f08
GillesR76 <gilles-richard76@hotmail.com>
2024-01-31 16:20:15 +0100

    divide 2 integers with exception

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    calculate = 0
+    calculate = None
     try:
         calculate = a / b
-        if a and b > 0:
-            print("Inside result: {}".format(float(calculate)))
-    except Exception:
-        print("Inside result: None")
+    except ZeroDivisionError:
         pass
     finally:
-        if a and b > 0:
-            return calculate
+        print("Inside result: {}".format(calculate))
+    return calculate
+
