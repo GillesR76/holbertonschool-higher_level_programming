44bff6cc52f1eecca0d84196b8f7b52155b58b56
GillesR76 <gilles-richard76@hotmail.com>
2024-02-01 05:56:40 +0100

    check if a function executes safely else stderr

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -6,7 +6,10 @@ def safe_function(fct, *args):
     try:
         if fct(*args):
             return fct(*args)
-    except Exception:
+    except IndexError:
         print("Exception: list index out of range",
               file=sys.stderr)
+        return None
+    except ZeroDivisionError:
+        print("Exception: division by zero", file=sys.stderr)
         return None
