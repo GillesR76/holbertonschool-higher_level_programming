2f6d1655fabd9aafad816b48b99f7d40950aa99e
GillesR76 <gilles-richard76@hotmail.com>
2024-02-02 11:18:16 +0100

    changing the skip line condition

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -101,7 +101,8 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            print("\n" * self.__position[1], end="")
+            for x in range(self.__position[1]):
+                print()
             for x in range(self.__size):
                 print(" " * self.__position[0], end="")
                 print("#" * self.__size)
