1ebf766a17a57f848ffbed2d9215cbaffac982dd
GillesR76 <gilles-richard76@hotmail.com>
2024-02-02 01:09:19 +0100

    add a private instance to class Square

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Write an empty class Square that defines a square
+"""
+
+
+class Square:
+    """
+    Empty class named Square
+    """
+    def __init__(self, size):
+        self.__size = size
