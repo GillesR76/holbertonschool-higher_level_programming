1b00c37de016c9aef449f708f4cf2b5718a24f3d
GillesR76 <gilles-richard76@hotmail.com>
2024-02-07 19:13:50 +0100

    adding the unittest file for max integer function

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 """Unittest for max_integer([..])
 """
+
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
-    
+
     def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
-        
+
     def test_max_integer(self):
         self.assertEqual(max_integer([1, 4, 8]), 8)
         self.assertEqual(max_integer([2, 9, 6, 7, 22]), 22)
         self.assertEqual(max_integer([-1, -5, -4]), -1)
 
-if __name__ == '__main__': 
-    unittest.main() 
+
+if __name__ == '__main__':
+    unittest.main()
