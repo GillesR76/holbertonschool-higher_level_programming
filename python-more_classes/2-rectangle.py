ecde386995e955e2ac1d201763aeadf27e7ecb89
GillesR76 <gilles-richard76@hotmail.com>
2024-02-07 20:34:23 +0100

    adding the 0 perimeter condition

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -92,4 +92,6 @@ class Rectangle:
         Returns:
             int: the perimeter of the rectangle
         """
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return (self.__width + self.__height) * 2
