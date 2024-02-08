ae1be02098561849baac0c195583d3bbdff23483
GillesR76 <gilles-richard76@hotmail.com>
2024-02-08 03:51:07 +0100

    removing parenthesis

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,7 +10,7 @@ def add_integer(a, b=98):
     """
     Function that adds two integers
     """
-    if not (isinstance(a, (int, float))):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
