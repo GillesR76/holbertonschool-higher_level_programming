a07df96e1aad33d7202d48a3c1e4925053c2ac24
GillesR76 <gilles-richard76@hotmail.com>
2024-02-12 14:52:59 +0100

    writing a class that inherits from a parent class

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+"""
+* Write a class MyList that inherits from list
+* Public instance method: def print_sorted(self):
+that prints the list, but sorted (ascending sort)
+* You can assume that all the elements of the list will be of type int
+* You are not allowed to import any module
+"""
+
+
+class MyList(list):
+    def print_sorted(self):
+        """
+        Function that prints the list, but sorted (ascending sort)
+        """
+        print(sorted(self))
