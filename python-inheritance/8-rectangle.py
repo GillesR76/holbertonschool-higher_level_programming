d46424352fd1cfdca6cb795cfa75926a84a37238
GillesR76 <gilles-richard76@hotmail.com>
2024-02-12 23:06:32 +0100

    modifying the integer verification in integer validator method

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+
+"""
+* Write a class BaseGeometry
+* Public instance method: that raises an Exception
+* Public instance method that validates value
+* You are not allowed to import any module
+"""
+
+
+class BaseGeometry:
+    def area(self):
+        """Public instance method that raises an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Public instance method that validates value
+        """
+        if not type(value) is int:
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+        
