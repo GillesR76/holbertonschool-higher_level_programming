a4a775b7b2f9411226735c3fa088e5090739417e
GillesR76 <gilles-richard76@hotmail.com>
2024-02-12 15:08:32 +0100

    function that verifies if an object is from a specific instance

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+"""
+* Write a function that returns True if the object
+is exactly an instance of the specified class ; otherwise False.
+* Prototype: def is_same_class(obj, a_class):
+* You are not allowed to import any module
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Function that returns True if the object is exactly
+    an instance of the specified class
+    """
+def is_same_class(obj, a_class):
+    return type(obj) is a_class
