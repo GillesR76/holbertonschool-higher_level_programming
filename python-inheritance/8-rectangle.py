4305039cd96bf93b39aeb91f2b6851f4af5fa6b6
GillesR76 <gilles-richard76@hotmail.com>
2024-02-12 23:11:55 +0100

    adding blank lines

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -10,23 +10,25 @@
 
 
 class BaseGeometry:
+    """class 'BaseGeometry'"""
+
     def area(self):
         """Public instance method that raises an exception"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-        Public instance method that validates value
-        """
+        """Public instance method that validates value"""
         if not type(value) is int:
             raise TypeError(f"{name} must be an integer")
         elif value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-        
+
+
 class Rectangle(BaseGeometry):
+    """New class Rectangle that inherits from parent class"""
+
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-        
