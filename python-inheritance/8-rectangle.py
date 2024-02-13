80fc9384614f106ea25693c2add533e5d42d69c5
GillesR76 <gilles-richard76@hotmail.com>
2024-02-13 02:25:49 +0100

    importing the parent class

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,33 +2,16 @@
 
 
 """
-* Write a class BaseGeometry
-* Public instance method: that raises an Exception
-* Public instance method that validates value
-* You are not allowed to import any module
+Import the BaseGeometry class
 """
-
-
-class BaseGeometry:
-    """class 'BaseGeometry'"""
-
-    def area(self):
-        """Public instance method that raises an exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Public instance method that validates value"""
-        if not type(value) is int:
-            raise TypeError(f"{name} must be an integer")
-        elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """New class Rectangle that inherits from parent class"""
 
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
