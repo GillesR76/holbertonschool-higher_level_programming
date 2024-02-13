2e45abe3f2c86305b8a775d60030bec7fe23f6d8
GillesR76 <gilles-richard76@hotmail.com>
2024-02-13 02:59:57 +0100

    function that reads a text file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+"""
+* Write a function that reads a text file (UTF8) and prints it to stdout:
+* Prototype: def read_file(filename=""):
+* You must use the with statement
+* You don’t need to manage file permission or file doesn't exist exceptions.
+* You are not allowed to import any module
+"""
+
+
+def read_file(filename=""):
+    """Function that reads a text file"""
+    with open("my_file_0.txt", encoding="utf-8") as filename:
+        print(filename.read())
