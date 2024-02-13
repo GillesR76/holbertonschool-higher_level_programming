d3c19e8dfea12f2282bf6c238af0befbe09ddc3b
GillesR76 <gilles-richard76@hotmail.com>
2024-02-13 10:20:00 +0100

    creating an object from a json file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,14 +5,14 @@
 * Write a function that creates an Object from a “JSON file”:
 * Prototype: def load_from_json_file(filename):
 * You must use the with statement
-* You don’t need to manage exceptions if the JSON string doesn’t represent an object.
-* You don’t need to manage file permissions / exceptions.
+* You dont need to manage exceptions if the JSON string doesnt represent an object.
+* You dont need to manage file permissions / exceptions.
 """
 
 import json
 
 
 def load_from_json_file(filename):
-    """Write a function that creates an Object from a JSON file"""
+    """Function that creates an Object from a JSON file"""
     with open(filename, mode="r", encoding="utf-8") as file:
         return json.load(file)
