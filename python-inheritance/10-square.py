202fca33cfba27904783556fe53d3e01ab17b8c0
GillesR76 <gilles-richard76@hotmail.com>
2024-02-13 01:37:07 +0100

    checking if size is a positive integer

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -47,6 +47,7 @@ class Square(Rectangle):
 
     def __init__(self, size):
         self.__size = size
+        super().integer_validator("size", size)
         super().__init__(self.__size, self.__size)
 
     def area(self):
