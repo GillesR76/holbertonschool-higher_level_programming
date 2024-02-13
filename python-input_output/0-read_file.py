b7e39c7f200e0746ba5e10ceb9b4363f7ca54df1
GillesR76 <gilles-richard76@hotmail.com>
2024-02-13 03:07:38 +0100

    removing the skip line in print function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -12,5 +12,5 @@
 
 def read_file(filename=""):
     """Function that reads a text file"""
-    with open("my_file_0.txt", encoding="utf-8") as filename:
-        print(filename.read(), end="")
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
