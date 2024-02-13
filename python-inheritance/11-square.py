6b8158ed2b849979c7907331d0f03159626fdf34
GillesR76 <gilles-richard76@hotmail.com>
2024-02-13 02:01:41 +0100

    adding the string inherited method

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Import the BaseGeometry class
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+"""
+Import the Rectangle class
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """New subclass Square that inherits from subclass Rectangle"""
+
+    def __init__(self, size):
+        self.__size = size
+        super().integer_validator("size", size)
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """Public instance method inherited from parent class"""
+        return self.__size * self.__size
+    
+    def __str__(self):
+        """String method that prints a reprsentation of a square"""
+        return f"[Rectangle] {self.__size}/{self.__size}"
