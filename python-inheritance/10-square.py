0b39845c87a63b1f0c29beaa1bd05a2cec322cea
GillesR76 <gilles-richard76@hotmail.com>
2024-02-13 01:48:00 +0100

    checking if size is a positive integer

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,45 +1,14 @@
 #!/usr/bin/python3
 
+"""
+Import the BaseGeometry class
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 """
-* Write a class BaseGeometry
-* Public instance method: that raises an Exception
-* Public instance method that validates value
-* You are not allowed to import any module
+Import the Rectangle class
 """
-
-
-class BaseGeometry:
-    """class 'BaseGeometry'"""
-
-    def area(self):
-        """Public instance method that raises an exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Public instance method that validates value"""
-        if not type(value) is int:
-            raise TypeError(f"{name} must be an integer")
-        elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """New class Rectangle that inherits from parent class"""
-
-    def __init__(self, width, height):
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """Public instance method inherited from parent class"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """String method"""
-        return f"[Rectangle] {self.__width}/{self.__height}"
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
