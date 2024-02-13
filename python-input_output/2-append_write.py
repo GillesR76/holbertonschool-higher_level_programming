d6acfce565fc7e0eabaaba8fdbbcee94d56bc1ee
GillesR76 <gilles-richard76@hotmail.com>
2024-02-13 03:23:59 +0100

    function that appends a string

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+"""
+* Write a function that writes a string to a text file (UTF8)
+and returns the number of characters written:
+* Prototype: def write_file(filename="", text=""):
+* You must use the with statement
+* You don’t need to manage file permission exceptions.
+* Your function should create the file if doesn’t exist.
+* Your function should overwrite the content of the file if it already exists.
+* You are not allowed to import any module
+"""
+
+
+def append_write(filename="", text=""):
+    """Function that writes a string to a text file"""
+    with open(filename, mode="a", encoding="utf-8") as file:
+        file.write(text)
+    return len(text)
