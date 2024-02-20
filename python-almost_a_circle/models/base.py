6e5b406f8c944e8e70035e6e71515e0c51155fe5
GillesR76 <gilles-richard76@hotmail.com>
2024-02-20 05:48:41 +0100

    adding a class method that writes a json string representation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -48,3 +48,11 @@ class Base:
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        if list_objs is None:
+            list_objs = []
+        list_dicts = [obj.to_dictionary() for obj in list_objs]
+        with open(cls.__name__ + ".json", "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(list_dicts))
