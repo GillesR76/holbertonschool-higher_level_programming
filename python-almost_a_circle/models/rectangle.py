478d7eb2b9010b6393beb68b33152c033bdd74f6
GillesR76 <gilles-richard76@hotmail.com>
2024-02-20 02:06:59 +0100

    assigning the kwargs attribute to the update method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -128,19 +128,17 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """
-        Public method that assigns an argument to each attribute
+        Public method that that assigns a key/value argument to attributes
         Args:
-            args: list of arguments using the unpacking operator *
+            args: list of no keyword arguments
+            kwargs: list of keyword arguments
         """
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+        if args and args is not None:
+            attributes = ["id", "width", "height", "x", "y"]
+            for attr, value in zip(attributes, args):
+                setattr(self, attr, value)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
