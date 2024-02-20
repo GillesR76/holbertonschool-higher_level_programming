204fa848512fe200670954c8668e96d7390df4f8
GillesR76 <gilles-richard76@hotmail.com>
2024-02-20 02:39:40 +0100

    implementing the new square instance

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+
+"""
+* Write the class Square that inherits from Rectangle:
+* In the file models/square.py
+* Class Square inherits from Rectangle
+* Class constructor: def __init__(self, size, x=0, y=0, id=None):
+* Call the super class with id, x, y, width and height - this super
+call will use the logic of the __init__ of the Rectangle class.
+The width and height must be assigned to the value of size
+* You must not create new attributes for this class, use all
+attributes of Rectangle - As reminder: a Square is a Rectangle
+with the same width and height
+* All width, height, x and y validation must inherit from Rectangle
+- same behavior in case of wrong data
+* The overloading __str__ method should return [Square] (<id>) <x>/<y>
+- <size> - in our case, width or height
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class Square that inherits from Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Constructor to initialize the Square instance
+
+        Args:
+            size (int): size of the square
+            x (int): coordinate of the square
+            y (int): coordinate of the square
+        """
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        """
+        String method that returns a value of type string
+
+        Returns:
+            str: a modified version of the str method
+        """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
