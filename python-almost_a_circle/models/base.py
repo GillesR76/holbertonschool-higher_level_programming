6fb04ccb89f66077f463362899f3538da65c2f62
GillesR76 <gilles-richard76@hotmail.com>
2024-02-20 09:48:32 +0100

    adding a static method that returns the list of a json string representation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -56,3 +56,9 @@ class Base:
         list_dicts = [obj.to_dictionary() for obj in list_objs]
         with open(cls.__name__ + ".json", "w", encoding="utf-8") as f:
             f.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if not json_string or json_string is None:
+            return "[]"
+        return json.loads(json_string)
