f6e1090b457fb015d0f30e9b545dd5bb6f34731b
GillesR76 <gilles-richard76@hotmail.com>
2024-02-21 21:08:32 +0100

    testing the square

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -5,9 +5,9 @@ from models.square import Square
 
 class TestSquare(unittest.TestCase):
     def test_square(self):
-        r1 = Square(1, 2)
-        self.assertEqual(r1.id, 1)
-        self.assertEqual(r1.x, 2)
+        s1 = Square(1, 2)
+        self.assertEqual(s1.size, 1)
+        self.assertEqual(s1.x, 2)
         
         with self.assertRaises(TypeError):
             r4 = Square("1", 2)
