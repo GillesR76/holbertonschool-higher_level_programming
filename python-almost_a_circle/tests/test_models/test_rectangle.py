422ffd217b520b841a4339025bc28346d2649351
GillesR76 <gilles-richard76@hotmail.com>
2024-02-21 15:42:01 +0100

    updating the rectangle test

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -6,10 +6,28 @@ from models.rectangle import Rectangle
 class TestRectangle(unittest.TestCase):
     
     def test_rectangle(self):
-        r1 = Rectangle(10, 2, 0, 0, 12)
-        self.assertEqual(r1.width, 10)
+        r1 = Rectangle(1, 2)
+        self.assertEqual(r1.width, 1)
         self.assertEqual(r1.height, 2)
-        self.assertEqual(r1.x, 0)
-        self.assertEqual(r1.y, 0)
-        self.assertEqual(r1.id, 12)
+        
+        r2 = Rectangle(10, 2, 0)
+        self.assertEqual(r2.width, 10)
+        self.assertEqual(r2.height, 2)
+        self.assertEqual(r2.x, 0)
+        
+        r3 = Rectangle(10, 2, 0, 0)
+        self.assertEqual(r3.width, 10)
+        self.assertEqual(r3.height, 2)
+        self.assertEqual(r3.x, 0)
+        self.assertEqual(r3.y, 0)
+        
+        with self.assertRaises(TypeError):
+            r4 = Rectangle("1", 2)
+        
+        r8 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r8.width, 10)
+        self.assertEqual(r8.height, 2)
+        self.assertEqual(r8.x, 0)
+        self.assertEqual(r8.y, 0)
+        self.assertEqual(r8.id, 12)
         
