90cbea85ca913c5631384e80c478e658416fca9f
GillesR76 <gilles-richard76@hotmail.com>
2024-03-20 17:14:16 +0100

    modifying the comments syntax for the checker

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,10 +1,9 @@
-/* Script that lists all shows contained in the database without a
-genre linked. Using a left join that returns all shows from the 
-tv shows table and the matched records from the tv show genre.
-Whith the left join, if there is no match the result is NULL on 
-the tv show genre side. Using the where/is null condition to filter
-only the results that return NULL
-*/
+-- Script that lists all shows contained in the database without a
+-- genre linked. Using a left join that returns all shows from the 
+-- tv shows table and the matched records from the tv show genre.
+-- Whith the left join, if there is no match the result is NULL on 
+-- the tv show genre side. Using the where/is null condition to filter
+-- only the results that return NULL
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
