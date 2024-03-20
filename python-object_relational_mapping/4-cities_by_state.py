f49db58de8bc4794249efc6399c6ec8d4f8fe121
GillesR76 <gilles-richard76@hotmail.com>
2024-03-20 09:58:08 +0100

    correcting the spacing on select line

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     cursor = connection.cursor()
     cursor.execute(
-        "SELECT cities.id, cities.name, states.name FROM cities INNER JOIN"
+        "SELECT cities.id, cities.name, states.name FROM cities INNER JOIN "
         "states ON cities.state_id = states.id ORDER BY cities.id")
 
     rows = cursor.fetchall()
