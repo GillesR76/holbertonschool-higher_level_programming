c3e8f9a45cd8d866ed31860c9353c2fde1124de2
GillesR76 <gilles-richard76@hotmail.com>
2024-03-20 11:49:54 +0100

    python file that contains the class definition of a State

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False,
+                unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
