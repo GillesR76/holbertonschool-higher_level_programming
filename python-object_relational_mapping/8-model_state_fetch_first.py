a198b6bab5b91a65e333c492c92b43508df0d9a4
GillesR76 <gilles-richard76@hotmail.com>
2024-03-20 13:02:22 +0100

    script that prints the first state object from the database

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+
+"""Write a script that prints the first State object
+from the database hbtn_0e_6_usa
+"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+    """including the guard clause"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    """creating a SQLAlchemy engine that will connect to the MySQL database"""
+
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    """ Query the database by selecting all rows from the
+    State table, ordered by the id column
+    """
+    state = session.query(State).first()
+    if not state:
+        print("Nothing ")
+    else:
+        print(f"{state.id}: {state.name}")
